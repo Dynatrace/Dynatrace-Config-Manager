9061e3f547836c43e1accbe4daf55a93a4d2d939
David Cryans <david.cryans@dynatrace.com>
2023-10-18 14:20:56 -0400

    fix small problem with port env. var Bring back manually testing connections

--- flask-backend/src/environment.py
+++ flask-backend/src/environment.py
@@ -7,16 +7,17 @@ DCM_PORT_ENV_VAR = os.getenv("DCM_PORT")
 DCM_PORT = FLASK_PORT
 
 try:
-    DCM_PORT = float(DCM_PORT_ENV_VAR)
     if DCM_PORT_ENV_VAR is None or DCM_PORT_ENV_VAR == "":
         print(
             f"The port number used: {FLASK_PORT} can be modified with the DCM_PORT environment variable"
         )
-    elif DCM_PORT < 0 or DCM_PORT > 65535:
-        print(f"Env. Var DCM_PORT is out of range [0-65535]: {DCM_PORT_ENV_VAR}")
     else:
-        print(f"Using Env. Var DCM_PORT, value: {FLASK_PORT}")
-        FLASK_PORT = DCM_PORT_ENV_VAR
+        DCM_PORT = float(DCM_PORT_ENV_VAR)
+        if DCM_PORT < 0 or DCM_PORT > 65535:
+            print(f"Env. Var DCM_PORT is out of range [0-65535]: {DCM_PORT_ENV_VAR}")
+        else:
+            print(f"Using Env. Var DCM_PORT, value: {FLASK_PORT}")
+            FLASK_PORT = DCM_PORT_ENV_VAR
 
 except ValueError:
     # If the input cannot be converted to a float, handle the exception
