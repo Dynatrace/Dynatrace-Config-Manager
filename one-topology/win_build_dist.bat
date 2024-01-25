d2fefec4766b152dfe25c9e22938cd485beb0e41
David Cryans <david.cryans@dynatrace.com>
2024-01-25 14:09:02 -0500

    add windows build commands for one-topology

---
+++ one-topology/win_build_dist.bat
@@ -0,0 +1,8 @@
+@echo off
+
+SET GOARCH=amd64
+SET GOOS=windows
+SET CGO_ENABLED=0
+
+go build -a -tags netgo -o ./build/one-topology-windows-amd64.exe ./cmd/monaco
+xcopy /Y /s build\one-topology-windows-amd64.exe ..\Dynatrace_Config_Manager-win64\one-topology\
