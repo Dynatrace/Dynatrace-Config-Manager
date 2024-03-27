9dd81150e10cb6f42ad8829b11e1eae61ddfce4a
dcryans <david.cryans@dynatrace.com>
2024-03-27 09:56:59 -0400

    modify sh scripts for easyjson

--- one-topology/build_win_linux.sh
+++ one-topology/build_win_linux.sh
@@ -1,3 +1,4 @@
+easyjson -all -omit_empty ~/repos/Dynatrace-Config-Manager/one-topology/pkg/match/entities/values/raw_entity_list.go
 env GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -a -tags netgo -ldflags '-X github.com/Dynatrace/Dynatrace-Config-Manager/one-topology/pkg/version.MonitoringAsCode=2.x -w -extldflags "-static"' -o ./build/one-topology-windows-amd64.exe ./cmd/monaco
 env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -a -tags netgo -ldflags '-X github.com/Dynatrace/Dynatrace-Config-Manager/one-topology/pkg/version.MonitoringAsCode=2.x -w -extldflags "-static"' -o ./build/one-topology-linux-amd64 ./cmd/monaco
 cp -p ./build/one-topology-linux-amd64 ../flask-backend/one-topology/one-topology-linux-amd64
--- one-topology/darwin_build_dist.sh
+++ one-topology/darwin_build_dist.sh
@@ -1,4 +1,4 @@
-easyjson -all -omit_empty C:\win-repos\Dynatrace-Config-Manager\one-topology\pkg\match\entities\values\testValuesList.go
+easyjson -all -omit_empty ~/repos/Dynatrace-Config-Manager/one-topology/pkg/match/entities/values/raw_entity_list.go
 env GOOS=darwin GOARCH=arm64 CGO_ENABLED=0 go build -a -tags netgo -ldflags '-X github.com/Dynatrace/Dynatrace-Config-Manager/one-topology/pkg/version.MonitoringAsCode=2.x -w -extldflags "-static"' -o ./build/one-topology-darwin-arm64 ./cmd/monaco
 mkdir -p ../Dynatrace_Config_Manager-darwin_arm64/one-topology/
 cp -p ./build/one-topology-darwin-arm64 ../Dynatrace_Config_Manager-darwin_arm64/one-topology/
